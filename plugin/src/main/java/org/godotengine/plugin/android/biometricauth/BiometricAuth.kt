982f2a524e8261403f76168ee8fff6d810ce7a4d
decentragri <decentragri@gmail.com>
2025-05-18 18:55:59 +0800

    chang activity source

--- plugin/src/main/java/org/godotengine/plugin/android/biometricauth/BiometricAuth.kt
+++ plugin/src/main/java/org/godotengine/plugin/android/biometricauth/BiometricAuth.kt
@@ -1,6 +1,5 @@
 package org.godotengine.plugin.android.biometricauth
 
-import android.os.Bundle
 import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
@@ -8,12 +7,16 @@ import org.godotengine.godot.Godot
 import org.godotengine.godot.plugin.GodotPlugin
 import org.godotengine.godot.plugin.SignalInfo
 import org.godotengine.godot.plugin.UsedByGodot
+import android.os.Handler
+import android.os.Looper
 
+
+@Suppress("unused")
 class BiometricAuth(private val godot: Godot) : GodotPlugin(godot) {
 
 	private var prompt: BiometricPrompt? = null
 
-	override fun getPluginName(): String = "BiometricAuthPlugin"
+	override fun getPluginName(): String = "BiometricAuth"
 
 	override fun getPluginSignals(): Set<SignalInfo> {
 		return setOf(
@@ -23,8 +26,9 @@ class BiometricAuth(private val godot: Godot) : GodotPlugin(godot) {
 	}
 
 	@UsedByGodot
+
 	fun startAuthentication() {
-		val activity = activity
+		val activity = godot.getActivity()
 		if (activity !is FragmentActivity) {
 			emitSignal("on_auth_failed", "Activity is not a FragmentActivity")
 			return
@@ -38,21 +42,23 @@ class BiometricAuth(private val godot: Godot) : GodotPlugin(godot) {
 			.setNegativeButtonText("Cancel")
 			.build()
 
-		prompt = BiometricPrompt(activity, executor, object : BiometricPrompt.AuthenticationCallback() {
-			override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-				emitSignal("on_auth_success")
-			}
+		Handler(Looper.getMainLooper()).post {
+			prompt = BiometricPrompt(activity, executor, object : BiometricPrompt.AuthenticationCallback() {
+				override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+					emitSignal("on_auth_success")
+				}
 
-			override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-				emitSignal("on_auth_failed", errString.toString())
-			}
+				override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+					emitSignal("on_auth_failed", errString.toString())
+				}
 
-			override fun onAuthenticationFailed() {
-				emitSignal("on_auth_failed", "Authentication attempt failed")
-			}
-		})
-
-		prompt?.authenticate(promptInfo)
+				override fun onAuthenticationFailed() {
+					emitSignal("on_auth_failed", "Authentication attempt failed")
+				}
+			})
+			prompt?.authenticate(promptInfo)
+		}
 	}
+
 
 }
